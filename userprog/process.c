8b7d2588292164287be976a9f47377a560020e01
kmj00204 <kmj00204@gmail.com>
2025-12-01 18:09:48 +0900

    test

--- userprog/process.c
+++ userprog/process.c
@@ -805,7 +805,7 @@ static bool load_segment(struct file* file, off_t ofs, uint8_t* upage, uint32_t 
     ASSERT((read_bytes + zero_bytes) % PGSIZE == 0);
     ASSERT(pg_ofs(upage) == 0);
     ASSERT(ofs % PGSIZE == 0);
-
+//
     while (read_bytes > 0 || zero_bytes > 0) {
         /* Do calculate how to fill this page.
          * We will read PAGE_READ_BYTES bytes from FILE
